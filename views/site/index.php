e8e0b9e42c29c3dceb35784aadc9e2a9cf68e791
kenny lau <kennyllau9@gmail.com>
2016-09-04 15:25:10 -0700

    templating webpage

---
+++ views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Hello!</h1>
+
+        <p><a class="btn btn-lg btn-success" href="#">lets make this site great!</a></p>
+    </div>
+
+</div>
